d02651c838ad07bd5795eb317f1565bf4954439b
PeterMaklakov <158668464+PeterMaklakov@users.noreply.github.com>
2024-03-24 11:07:52 +1300

    Add files via upload

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import NavBar from "../../common/NavBar"
+import Footer from '../../common/Footer'
+import styles from "./Home.module.css"
+import HomeDashboard from '../HomeDashboard/HomeDashboard'
 
 export default function Home() {
   return (
     <>
-        <NavBar/>
-        <br/>
-        <div>Home</div>
+      <NavBar />
+      
+      <div class={styles.container}>
+        <div className='prepare'>
+
+        <h1>Prepare young minds</h1>
+        <h1>for a better <span>future.</span></h1 >
+        <h3>Let us help you advance students in Digital</h3>
+        <h3>Technologies and other learning areas with</h3>
+        <h3>Our project-based learning programme.</h3>
+          
+        <button className={styles.learnMoreButton}>LEARN MORE</button>
+        <button className={styles.signUpButton}>SIGN UP</button>
+        </div>
+
+      </div>
+
+      <HomeDashboard/>
+      <Footer />
     </>
-    
   )
 }
